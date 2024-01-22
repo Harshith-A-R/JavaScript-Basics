80b00eae51fca1c2ffaadd490d214b630bf1f08d
Harshith A R <100902368+Harshith-A-R@users.noreply.github.com>
2024-01-22 17:53:17 +0000

    variables done

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,33 @@
+/*const accountId =12344 
+//accountId =1561
+console.log(accountId);
+
+let name ="Harshith"
+var age =27
+height =6
+
+console.table([accountId,name,age,height])*/
+
+
+const accountId = 144553
+let accountEmail = "harshith@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState =null
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
