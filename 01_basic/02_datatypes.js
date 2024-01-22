683be87b25166c34936f24f77cfe13312da62e21
Harshith A R <100902368+Harshith-A-R@users.noreply.github.com>
2024-01-22 19:09:09 +0000

    datatypes done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+let name ="Harshith"
+let age=25
+let isLogin=false
+let state;
+
+//data types 
+/* Below are Primitive Data types */
+
+//number - range will be 2 to power 53
+//string
+//BigInt
+//boolean
+//undifined 
+//symbol - to find unique component /identifying unique component
+
+//object
+console.log(typeof null)
