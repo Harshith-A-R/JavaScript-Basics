63e540dbb44b50f8e66fa7674c53c557d746c4f5
Harshith A R <100902368+Harshith-A-R@users.noreply.github.com>
2024-01-23 19:53:51 +0000

    updated

--- 01_basic/03_conversion_of_datatypes.js
+++ 01_basic/03_conversion_of_datatypes.js
@@ -1,5 +1,25 @@
-let number =true
-
+let number =undefined
+//here converting string boolean to number
 let valueNumber =Number(number)
 console.log(typeof valueNumber)
-console.log(typeof number)
+console.log(valueNumber)
+
+//"33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//null=> 0
+
+//let isLogin ="Harshith" //true
+//let isLogin =1 true 
+//let isLogin =0 false
+let isLogin ="" //false
+
+
+let isLoggedin =Boolean(isLogin)
+console.log(typeof isLoggedin)
+console.log(isLoggedin)
+
+let string =33
+let stringValue=String(string)
+console.log(typeof stringValue)
+console.log(stringValue)
