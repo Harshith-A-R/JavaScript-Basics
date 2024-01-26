130b0014fc189d85fa9a4afa0760e77634bb8a5e
Harshith A R <100902368+Harshith-A-R@users.noreply.github.com>
2024-01-26 16:52:39 +0000

    stack and heap memory

---
+++ 01_basic/05_stackAndHeapmemory.js
@@ -0,0 +1,10 @@
+//Stack(primitive), Heap (non primitive)
+//Stack memory :the primitive data will store in stack memory,
+let myName="Harshith"
+
+let myName2=myName
+
+myName2="Harsha"
+console.log(myName)
+console.log(myName2)
+
